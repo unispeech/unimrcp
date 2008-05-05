6a450ea5e453dc14c007bffa7d7c35dd4c501506
Arsen Chaloyan <achaloyan@gmail.com>
2008-05-05 19:58:54 +0000

    added rtp header and rtp stream descriptor definitions

---
+++ libs/mpf/include/mpf_rtp_stream_descriptor.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MPF_RTP_STREAM_DESCRIPTOR_H__
+#define __MPF_RTP_STREAM_DESCRIPTOR_H__
+
+/**
+ * @file mpf_rtp_stream_descriptor.h
+ * @brief MPF RTP Stream Descriptor
+ */ 
+
+#include "mpf.h"
+
+APT_BEGIN_EXTERN_C
+
+typedef struct mpf_rtp_media_descriptor_t mpf_rtp_media_descriptor_t;
+/** RTP media (local/remote) descriptor */
+struct mpf_rtp_media_descriptor_t {
+	const char      *ip;
+	apr_port_t       port;
+
+	mpf_codec_list_t codec_list;
+};
+
+typedef struct mpf_rtp_stream_descriptor_t mpf_rtp_stream_descriptor_t;
+/** RTP stream descriptor */
+struct mpf_rtp_stream_descriptor_t {
+	mpf_rtp_media_descriptor_t local;
+	mpf_rtp_media_descriptor_t remote;
+};
+
+typedef struct mpf_rtp_termination_descriptor_t mpf_rtp_termination_descriptor_t;
+/** RTP termination descriptor */
+struct mpf_rtp_termination_descriptor_t {
+	mpf_rtp_stream_descriptor_t audio;
+	mpf_rtp_stream_descriptor_t video;
+};
+
+
+APT_END_EXTERN_C
+
+#endif /*__MPF_RTP_STREAM_DESCRIPTOR_H__*/
