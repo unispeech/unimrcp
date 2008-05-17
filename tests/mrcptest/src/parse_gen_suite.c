4c98e06088b7e974c8e076d9c24343ee12a76732
Arsen Chaloyan <achaloyan@gmail.com>
2008-05-17 18:50:16 +0000

    added empty test project for mrcp parser

---
+++ tests/mrcptest/src/parse_gen_suite.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "apt_test_suite.h"
+#include "apt_log.h"
+#include "mrcp_resource_factory.h"
+
+static apt_bool_t parse_gen_test_run(apt_test_suite_t *suite, int argc, const char * const *argv)
+{
+	return TRUE;
+}
+
+apt_test_suite_t* parse_gen_test_suite_create(apr_pool_t *pool)
+{
+	apt_test_suite_t *suite = apt_test_suite_create(pool,"parse-gen",NULL,parse_gen_test_run);
+	return suite;
+}
