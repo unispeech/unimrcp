89a5df06a6cdebb4df32880d821cdb7b57871c52
Arsen Chaloyan <achaloyan@gmail.com>
2008-05-24 19:07:03 +0000

    initially added unimrcpserver application

---
+++ platforms/unimrcp-server/src/main.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "unimrcp_server.h"
+
+int main(int argc, const char * const *argv)
+{
+	mrcp_server_t *server;
+	
+	if(apr_initialize() != APR_SUCCESS) {
+		apr_terminate();
+		return 0;
+	}
+
+	server = unimrcp_server_start();
+	if(server) {
+		getchar();
+		unimrcp_server_shutdown(server);
+	}
+
+	apr_terminate();
+	return 0;
+}
