8189c192393165a444b325511a35d3ca987c41f9
Arsen Chaloyan <achaloyan@gmail.com>
2008-05-27 04:35:41 +0000

    using mrcp-sofiasip module as an implementation of signaling interface for mrcpv2 in unimrcp server

--- platforms/libunimrcp-server/src/unimrcp_server.c
+++ platforms/libunimrcp-server/src/unimrcp_server.c
@@ -17,7 +17,11 @@
 #include "unimrcp_server.h"
 #include "mrcp_default_factory.h"
 #include "mpf_engine.h"
+#include "mrcp_sofiasip_agent.h"
 #include "apt_log.h"
+
+static mrcp_sig_agent_t* mrcpv2_sig_agent_create(apr_pool_t *pool);
+static mrcp_sig_agent_t* mrcpv1_sig_agent_create(apr_pool_t *pool);
 
 /** Start UniMRCP server */
 MRCP_DECLARE(mrcp_server_t*) unimrcp_server_start()
@@ -25,6 +29,7 @@ MRCP_DECLARE(mrcp_server_t*) unimrcp_server_start()
 	apr_pool_t *pool;
 	mrcp_resource_factory_t *resource_factory;
 	mpf_engine_t *media_engine;
+	mrcp_sig_agent_t *sig_agent;
 	mrcp_server_t *server = mrcp_server_create();
 	if(!server) {
 		return NULL;
@@ -40,6 +45,10 @@ MRCP_DECLARE(mrcp_server_t*) unimrcp_server_start()
 		apt_task_t *media_task = mpf_task_get(media_engine);
 		mrcp_server_media_engine_register(server,media_task);
 	}
+	sig_agent = mrcpv2_sig_agent_create(pool);
+	if(sig_agent) {
+		mrcp_server_signaling_agent_register(server,sig_agent);
+	}
 
 	mrcp_server_start(server);
 	return server;
@@ -52,4 +61,18 @@ MRCP_DECLARE(apt_bool_t) unimrcp_server_shutdown(mrcp_server_t *server)
 		return FALSE;
 	}
 	return mrcp_server_destroy(server);
+}
+
+static mrcp_sig_agent_t* mrcpv2_sig_agent_create(apr_pool_t *pool)
+{
+	mrcp_sofia_config_t *config = mrcp_sofiasip_config_alloc(pool);
+	config->local_ip = "127.0.0.1";
+	config->local_port = 8060;
+	config->user_agent_name = "UniMRCP Sofia-SIP";
+	return mrcp_sofiasip_agent_create(config,pool);
+}
+
+static mrcp_sig_agent_t* mrcpv1_sig_agent_create(apr_pool_t *pool)
+{
+	return NULL;
 }
