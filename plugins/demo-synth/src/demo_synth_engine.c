181ee016f87fe974583d045761b34a94361fbca8
Arsen Chaloyan <achaloyan@gmail.com>
2008-07-29 13:25:46 +0000

    implementing demo synthesizer engine (plugin)

--- plugins/demo-synth/src/demo_synth_engine.c
+++ plugins/demo-synth/src/demo_synth_engine.c
@@ -17,6 +17,11 @@
 #include "demo_synth_engine.h"
 #include "mrcp_synth_resource.h"
 #include "mrcp_message.h"
+#include "apt_consumer_task.h"
+
+typedef struct demo_synth_engine_t demo_synth_engine_t;
+typedef struct demo_synth_channel_t demo_synth_channel_t;
+typedef struct demo_synth_msg_t demo_synth_msg_t;
 
 /** Declaration of synthesizer engine methods */
 static apt_bool_t demo_synth_engine_destroy(mrcp_resource_engine_t *engine);
@@ -45,12 +50,6 @@ static const struct mrcp_engine_channel_method_vtable_t channel_vtable = {
 	demo_synth_channel_request_process
 };
 
-/** Declaration of synthesizer audio stream */
-typedef struct demo_synth_stream_t demo_synth_stream_t;
-struct demo_synth_stream_t {
-	mpf_audio_stream_t base;
-};
-
 /** Declaration of synthesizer stream methods */
 static apt_bool_t demo_synth_stream_destroy(mpf_audio_stream_t *stream);
 static apt_bool_t demo_synth_stream_open(mpf_audio_stream_t *stream);
@@ -67,41 +66,101 @@ static const mpf_audio_stream_vtable_t audio_stream_vtable = {
 	NULL
 };
 
+/** Declaration of demo synthesizer engine */
+struct demo_synth_engine_t {
+	apt_consumer_task_t    *task;
+};
+
+/** Declaration of demo synthesizer channel */
+struct demo_synth_channel_t {
+	mpf_audio_stream_t   audio_stream;
+	demo_synth_engine_t *demo_engine;
+
+	apt_bool_t           speaking;
+};
+
+/**  */
+typedef enum {
+	DEMO_SYNTH_MSG_OPEN_CHANNEL,
+	DEMO_SYNTH_MSG_CLOSE_CHANNEL,
+	DEMO_SYNTH_MSG_REQUEST_PROCESS
+} demo_synth_msg_type_e;
+
+/** Declaration of demo synthesizer task message */
+struct demo_synth_msg_t {
+	demo_synth_msg_type_e  type;
+	mrcp_engine_channel_t *channel; 
+	mrcp_message_t        *request;
+};
+
+static apt_bool_t demo_synth_msg_signal(demo_synth_msg_type_e type, mrcp_engine_channel_t *channel, mrcp_message_t *request);
+static apt_bool_t demo_synth_msg_process(apt_task_t *task, apt_task_msg_t *msg);
 
 /** Create demo synthesizer engine */
 MRCP_PLUGIN_DECLARE(mrcp_resource_engine_t*) demo_synth_engine_create(apr_pool_t *pool)
 {
-	mrcp_resource_engine_t *engine = mrcp_resource_engine_create(MRCP_SYNTHESIZER_RESOURCE,NULL,&engine_vtable,pool);
+	mrcp_resource_engine_t *engine;
+
+	demo_synth_engine_t *demo_engine = apr_palloc(pool,sizeof(demo_synth_engine_t));
+	apt_task_vtable_t task_vtable;
+	apt_task_msg_pool_t *msg_pool;
+
+	apt_task_vtable_reset(&task_vtable);
+	task_vtable.process_msg = demo_synth_msg_process;
+	msg_pool = apt_task_msg_pool_create_dynamic(sizeof(demo_synth_msg_t),pool);
+	demo_engine->task = apt_consumer_task_create(demo_engine,&task_vtable,msg_pool,pool);
+
+	engine = mrcp_resource_engine_create(MRCP_SYNTHESIZER_RESOURCE,demo_engine,&engine_vtable,pool);
 	return engine;
 }
 
+/** Destroy synthesizer engine */
 static apt_bool_t demo_synth_engine_destroy(mrcp_resource_engine_t *engine)
 {
-	/* destroy synthesizer engine */
+	demo_synth_engine_t *demo_engine = engine->obj;
+	if(demo_engine->task) {
+		apt_task_t *task = apt_consumer_task_base_get(demo_engine->task);
+		apt_task_destroy(task);
+		demo_engine->task = NULL;
+	}
 	return TRUE;
 }
 
+/** Open synthesizer engine */
 static apt_bool_t demo_synth_engine_open(mrcp_resource_engine_t *engine)
 {
-	/* open synthesizer engine */
+	demo_synth_engine_t *demo_engine = engine->obj;
+	if(demo_engine->task) {
+		apt_task_t *task = apt_consumer_task_base_get(demo_engine->task);
+		apt_task_start(task);
+	}
 	return TRUE;
 }
 
+/** Close synthesizer engine */
 static apt_bool_t demo_synth_engine_close(mrcp_resource_engine_t *engine)
 {
-	/* close synthesizer engine */
+	demo_synth_engine_t *demo_engine = engine->obj;
+	if(demo_engine->task) {
+		apt_task_t *task = apt_consumer_task_base_get(demo_engine->task);
+		apt_task_terminate(task,TRUE);
+	}
 	return TRUE;
 }
 
 static mrcp_engine_channel_t* demo_synth_engine_channel_create(mrcp_resource_engine_t *engine, apr_pool_t *pool)
 {
 	mpf_termination_t *termination;
-	demo_synth_stream_t *synth_stream = apr_palloc(pool,sizeof(demo_synth_stream_t));
-	mpf_audio_stream_init(&synth_stream->base,&audio_stream_vtable);
-	synth_stream->base.mode = STREAM_MODE_RECEIVE;
+	mpf_audio_stream_t *audio_stream;
+	demo_synth_channel_t *synth_channel = apr_palloc(pool,sizeof(demo_synth_channel_t));
+	synth_channel->demo_engine = engine->obj;
+	synth_channel->speaking = FALSE;
+	audio_stream = &synth_channel->audio_stream;
+	mpf_audio_stream_init(audio_stream,&audio_stream_vtable);
+	audio_stream->mode = STREAM_MODE_RECEIVE;
 	
-	termination = mpf_raw_termination_create(NULL,&synth_stream->base,NULL,pool);
-	return mrcp_engine_channel_create(engine,&channel_vtable,NULL,termination,pool);
+	termination = mpf_raw_termination_create(NULL,audio_stream,NULL,pool);
+	return mrcp_engine_channel_create(engine,&channel_vtable,synth_channel,termination,pool);
 }
 
 static apt_bool_t demo_synth_channel_destroy(mrcp_engine_channel_t *channel)
@@ -111,31 +170,42 @@ static apt_bool_t demo_synth_channel_destroy(mrcp_engine_channel_t *channel)
 
 static apt_bool_t demo_synth_channel_open(mrcp_engine_channel_t *channel)
 {
-	/* open channel and send asynch response */
-	return mrcp_engine_channel_open_respond(channel,TRUE);
+	return demo_synth_msg_signal(DEMO_SYNTH_MSG_OPEN_CHANNEL,channel,NULL);
 }
 
 static apt_bool_t demo_synth_channel_close(mrcp_engine_channel_t *channel)
 {
-	/* close channel, make sure there is no activity and send asynch response */
-	return mrcp_engine_channel_close_respond(channel);
+	return demo_synth_msg_signal(DEMO_SYNTH_MSG_CLOSE_CHANNEL,channel,NULL);
+}
+
+static apt_bool_t demo_synth_channel_request_process(mrcp_engine_channel_t *channel, mrcp_message_t *request)
+{
+	return demo_synth_msg_signal(DEMO_SYNTH_MSG_REQUEST_PROCESS,channel,request);
 }
 
 static apt_bool_t demo_synth_channel_speak(mrcp_engine_channel_t *channel, mrcp_message_t *request)
 {
 	/* process speak request, and send asynch response */
-	mrcp_message_t *response = mrcp_response_create(request,request->pool);
+	mrcp_message_t *response;
+	demo_synth_channel_t *synth_channel = channel->method_obj;
+	synth_channel->speaking = TRUE;
+	
+	response = mrcp_response_create(request,request->pool);
 	return mrcp_engine_channel_message_send(channel,response);
 }
 
 static apt_bool_t demo_synth_channel_stop(mrcp_engine_channel_t *channel, mrcp_message_t *request)
 {
 	/* process stop request, and send asynch response */
-	mrcp_message_t *response = mrcp_response_create(request,request->pool);
+	mrcp_message_t *response;
+	demo_synth_channel_t *synth_channel = channel->method_obj;
+	synth_channel->speaking = TRUE;
+
+	response = mrcp_response_create(request,request->pool);
 	return mrcp_engine_channel_message_send(channel,response);
 }
 
-static apt_bool_t demo_synth_channel_request_process(mrcp_engine_channel_t *channel, mrcp_message_t *request)
+static apt_bool_t demo_synth_channel_request_dispatch(mrcp_engine_channel_t *channel, mrcp_message_t *request)
 {
 	apt_bool_t status = FALSE;
 	switch(request->start_line.method_id) {
@@ -182,5 +252,51 @@ static apt_bool_t demo_synth_stream_close(mpf_audio_stream_t *stream)
 
 static apt_bool_t demo_synth_stream_read(mpf_audio_stream_t *stream, mpf_frame_t *frame)
 {
+	demo_synth_channel_t *synth_channel = (demo_synth_channel_t*)stream;
+	if(synth_channel->speaking == TRUE) {
+		frame->type |= MEDIA_FRAME_TYPE_AUDIO;
+		memset(frame->codec_frame.buffer,0,frame->codec_frame.size);
+	}
+	return TRUE;
+}
+
+static apt_bool_t demo_synth_msg_signal(demo_synth_msg_type_e type, mrcp_engine_channel_t *channel, mrcp_message_t *request)
+{
+	apt_bool_t status = FALSE;
+	demo_synth_channel_t *demo_channel = channel->method_obj;
+	demo_synth_engine_t *demo_engine = demo_channel->demo_engine;
+	apt_task_t *task = apt_consumer_task_base_get(demo_engine->task);
+	apt_task_msg_t *msg = apt_task_msg_get(task);
+	if(msg) {
+		demo_synth_msg_t *demo_msg;
+		msg->type = TASK_MSG_USER;
+		demo_msg = (demo_synth_msg_t*) msg->data;
+
+		demo_msg->type = type;
+		demo_msg->channel = channel;
+		demo_msg->request = request;
+		status = apt_task_msg_signal(task,msg);
+	}
+	return status;
+}
+
+static apt_bool_t demo_synth_msg_process(apt_task_t *task, apt_task_msg_t *msg)
+{
+	demo_synth_msg_t *demo_msg = (demo_synth_msg_t*)msg->data;
+	switch(demo_msg->type) {
+		case DEMO_SYNTH_MSG_OPEN_CHANNEL:
+			/* open channel and send asynch response */
+			mrcp_engine_channel_open_respond(demo_msg->channel,TRUE);
+			break;
+		case DEMO_SYNTH_MSG_CLOSE_CHANNEL:
+			/* close channel, make sure there is no activity and send asynch response */
+			mrcp_engine_channel_close_respond(demo_msg->channel);
+			break;
+		case DEMO_SYNTH_MSG_REQUEST_PROCESS:
+			demo_synth_channel_request_dispatch(demo_msg->channel,demo_msg->request);
+			break;
+		default:
+			break;
+	}
 	return TRUE;
 }
