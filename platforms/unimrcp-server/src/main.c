974fab858add9ee2c4e0e73c651600cdb96925f9
Arsen Chaloyan <achaloyan@gmail.com>
2008-10-27 22:07:52 +0000

    initial steps in running server as windows service

--- platforms/unimrcp-server/src/main.c
+++ platforms/unimrcp-server/src/main.c
@@ -71,6 +71,58 @@ static apt_bool_t cmdline_run()
 	return TRUE;
 }
 
+#ifdef WIN32
+#include <windows.h>
+static void service_cmd_process(int optch)
+{
+	SC_HANDLE schManager = OpenSCManager(0,0,SC_MANAGER_ALL_ACCESS);
+	if(!schManager) {
+		return;
+	}
+
+	switch(optch) {
+		case 'r':
+		{
+			SC_HANDLE schService;
+			char binPath[MAX_PATH]; 
+			if(!GetModuleFileName(NULL,binPath,MAX_PATH)) {
+				return;
+			}
+			schService = CreateService(
+							schManager,
+							"unimrcp",
+							"UniMRCP Server",
+							GENERIC_EXECUTE,
+							SERVICE_WIN32_OWN_PROCESS,
+							SERVICE_DEMAND_START,
+							SERVICE_ERROR_NORMAL,
+							binPath,0,0,0,0,0);
+			if(schService) {
+				CloseServiceHandle(schService);
+			}
+			break;
+		}
+		case 'u':
+		{
+			SC_HANDLE schService = OpenService(schManager,"unimrcp",DELETE|SERVICE_STOP);
+			if(schService) {
+				ControlService(schService,SERVICE_CONTROL_STOP,0);
+				DeleteService(schService);
+				CloseServiceHandle(schService);
+			}
+			break;
+		}
+		case 's':
+			break;
+		case 'o':
+			break;
+		default:
+			break;
+	}
+	CloseServiceHandle(schManager);
+}
+#endif
+
 static void usage()
 {
 	printf(
@@ -87,6 +139,16 @@ static void usage()
 		"\n"
 		"   -l [--log] priority    : Set the log priority (0-emergency, ..., 7-debug).\n"
 		"\n"
+#ifdef WIN32
+		"   -r [--register]        : Register the Windows service.\n"
+		"\n"
+		"   -u [--unregister]      : Unregister the Windows service.\n"
+		"\n"
+		"   -s [--start]           : Start the Windows service.\n"
+		"\n"
+		"   -o [--stop]            : Stop the Windows service.\n"
+		"\n"
+#endif
 		"   -h [--help]            : Show the help.\n"
 		"\n");
 }
@@ -103,6 +165,12 @@ static apt_bool_t options_load(const char **conf_dir_path, const char **plugin_d
 		{ "conf-dir",    'c', TRUE,  "path to config dir" },/* -c arg or --conf-dir arg */
 		{ "plugin-dir",  'p', TRUE,  "path to plugin dir" },/* -p arg or --plugin-dir arg */
 		{ "log",         'l', TRUE,  "log priority" },      /* -l arg or --log arg */
+#ifdef WIN32
+		{ "register",    'r', FALSE, "register service" },  /* -r or --register */
+		{ "unregister",  'u', FALSE, "unregister service" },/* -u or --unregister */
+		{ "start",       's', FALSE, "start service" },     /* -s or --start */
+		{ "stop",        'o', FALSE, "stop service" },      /* -o or --stop */
+#endif
 		{ "help",        'h', FALSE, "show help" },         /* -h or --help */
 		{ NULL, 0, 0, NULL },                               /* end */
 	};
@@ -132,6 +200,14 @@ static apt_bool_t options_load(const char **conf_dir_path, const char **plugin_d
 					apt_log_priority_set(atoi(optarg));
 				}
 				break;
+#ifdef WIN32
+			case 'r':
+			case 'u':
+			case 's':
+			case 'o':
+				service_cmd_process(optch);
+				return FALSE;
+#endif
 			case 'h':
 				usage();
 				return FALSE;
