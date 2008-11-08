31bae36bf695a02acdb19c31086274fb147eb78b
Arsen Chaloyan <achaloyan@gmail.com>
2008-11-08 07:59:15 +0000

    added apt_dir_layout to represent the generic dir layout structure

---
+++ libs/apr-toolkit/include/apt_dir_layout.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __APT_DIR_LAYOUT_H__
+#define __APT_DIR_LAYOUT_H__
+
+/**
+ * @file apt_dir_layout.h
+ * @brief Directory Layout
+ */ 
+
+#include "apt.h"
+
+APT_BEGIN_EXTERN_C
+
+/** Dir layout declaration */
+typedef struct apt_dir_layout_t apt_dir_layout_t;
+
+/** Dir layout */
+struct apt_dir_layout_t {
+	char *conf_dir_path;
+	char *plugin_dir_path;
+	char *log_dir_path;
+	char *data_dir_path;
+};
+
+/**
+ * Create (allocate) the structure of default directories layout.
+ */
+APT_DECLARE(apt_dir_layout_t*) apt_default_dir_layout_create(const char *base_dir_path, apr_pool_t *pool);
+
+/**
+ * Create (allocate) the structure of custom directories layout.
+ */
+APT_DECLARE(apt_dir_layout_t*) apt_custom_dir_layout_create(
+									const char *conf_dir_path,
+									const char *plugin_dir_path,
+									const char *log_dir_path,
+									const char *data_dir_path,
+									apr_pool_t *pool);
+
+
+APT_END_EXTERN_C
+
+#endif /*__APT_DIR_LAYOUT_H__*/
---
+++ libs/apr-toolkit/src/apt_dir_layout.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <apr_file_info.h>
+#include "apt_dir_layout.h"
+
+APT_DECLARE(apt_dir_layout_t*) apt_default_dir_layout_create(const char *base_dir_path, apr_pool_t *pool)
+{
+	apt_dir_layout_t *dir_layout = (apt_dir_layout_t*) apr_palloc(pool,sizeof(apt_dir_layout_t));
+	apr_filepath_merge(&dir_layout->conf_dir_path,base_dir_path,"conf",0,pool);
+	apr_filepath_merge(&dir_layout->plugin_dir_path,base_dir_path,"plugin",0,pool);
+	apr_filepath_merge(&dir_layout->log_dir_path,base_dir_path,"log",0,pool);
+	apr_filepath_merge(&dir_layout->data_dir_path,base_dir_path,"data",0,pool);
+	return dir_layout;
+}
+
+APT_DECLARE(apt_dir_layout_t*) apt_custom_dir_layout_create(
+									const char *conf_dir_path,
+									const char *plugin_dir_path,
+									const char *log_dir_path,
+									const char *data_dir_path,
+									apr_pool_t *pool)
+{
+	apt_dir_layout_t *dir_layout = (apt_dir_layout_t*) apr_palloc(pool,sizeof(apt_dir_layout_t));
+	dir_layout->conf_dir_path = apr_pstrdup(pool,conf_dir_path);
+	dir_layout->plugin_dir_path = apr_pstrdup(pool,plugin_dir_path);
+	dir_layout->log_dir_path = apr_pstrdup(pool,log_dir_path);
+	dir_layout->data_dir_path = apr_pstrdup(pool,data_dir_path);
+	return dir_layout;
+}
