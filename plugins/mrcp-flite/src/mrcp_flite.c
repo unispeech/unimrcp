8b1fd50db3d581cca7d358103fd77bf04b9330b4
Garmt Noname <garmt.noname@gmail.com>
2009-06-30 10:38:54 +0000

    Added resampling to 8kHz so that it works with freeswitch (specify only L16/96/8000 codec in profile of media_engine/rtpfactory). Changed logging to DEBUG level rather than INFO. Added channel_guard for stop response.

--- plugins/mrcp-flite/src/mrcp_flite.c
+++ plugins/mrcp-flite/src/mrcp_flite.c
@@ -74,9 +74,6 @@ static const struct mrcp_engine_channel_method_vtable_t channel_vtable = {
 	flite_synth_channel_request_process
 };
 
-
-
-
 /** Declaration of synthesizer audio stream methods */
 static apt_bool_t flite_synth_stream_destroy(mpf_audio_stream_t *stream);
 static apt_bool_t flite_synth_stream_open(mpf_audio_stream_t *stream);
@@ -104,17 +101,14 @@ struct flite_synth_engine_t {
 	} voices;
 };
 
-// flite stuff
+/** declarations for flite voices **/
 cst_voice *register_cmu_us_awb(void);
-void unregister_cmu_us_awb(cst_voice * v);
-
 cst_voice *register_cmu_us_kal(void);
-void unregister_cmu_us_kal(cst_voice * v);
-
 cst_voice *register_cmu_us_rms(void);
-void unregister_cmu_us_rms(cst_voice * v);
-
 cst_voice *register_cmu_us_slt(void);
+void unregister_cmu_us_awb(cst_voice * v);
+void unregister_cmu_us_kal(cst_voice * v);
+void unregister_cmu_us_rms(cst_voice * v);
 void unregister_cmu_us_slt(cst_voice * v);
 
 
@@ -130,6 +124,7 @@ struct flite_synth_channel_t {
 	cst_voice				*voice;
 	apr_pool_t				*pool;
 	apt_consumer_task_t     *task;
+	apr_thread_mutex_t	    *channel_guard;
 };
 
 /** Declaration of flite synthesizer task message */
@@ -166,7 +161,7 @@ MRCP_PLUGIN_DECLARE(mrcp_resource_engine_t*) mrcp_plugin_create(apr_pool_t *pool
 /** Destroy synthesizer engine */
 static apt_bool_t flite_synth_engine_destroy(mrcp_resource_engine_t *engine)
 {
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_engine_destroy");
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_engine_destroy");
 	return TRUE;
 }
 
@@ -174,7 +169,7 @@ static apt_bool_t flite_synth_engine_destroy(mrcp_resource_engine_t *engine)
 static apt_bool_t flite_synth_engine_open(mrcp_resource_engine_t *engine)
 {
 	flite_synth_engine_t *flite_engine = (flite_synth_engine_t *) engine->obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_engine_open");
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_engine_open");
 
 	flite_init();
 	flite_engine->voices.awb = register_cmu_us_awb();
@@ -182,7 +177,7 @@ static apt_bool_t flite_synth_engine_open(mrcp_resource_engine_t *engine)
 	flite_engine->voices.rms = register_cmu_us_rms();
 	flite_engine->voices.slt = register_cmu_us_slt();
 
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "fliteInitialize success");
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite init success");
 
 	return TRUE;
 }
@@ -191,7 +186,7 @@ static apt_bool_t flite_synth_engine_open(mrcp_resource_engine_t *engine)
 static apt_bool_t flite_synth_engine_close(mrcp_resource_engine_t *engine)
 {
 	flite_synth_engine_t *flite_engine = (flite_synth_engine_t *) engine->obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_engine_close");
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_engine_close");
 
 	unregister_cmu_us_awb(flite_engine->voices.awb);
 	unregister_cmu_us_kal(flite_engine->voices.kal);
@@ -208,14 +203,14 @@ static mrcp_engine_channel_t* flite_synth_engine_channel_create(mrcp_resource_en
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) apr_palloc(pool,sizeof(flite_synth_channel_t));
 	mpf_codec_descriptor_t *codec_descriptor = NULL;
 
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_engine_channel_create");
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_engine_channel_create");
 
-	codec_descriptor = (mpf_codec_descriptor_t *) apr_palloc(pool,sizeof(mpf_codec_descriptor_t));
-	mpf_codec_descriptor_init(codec_descriptor);
-	codec_descriptor->channel_count = 1;
-	codec_descriptor->payload_type = 96;
-	apt_string_set(&codec_descriptor->name,"LPCM");
-	codec_descriptor->sampling_rate = 16000;
+//	codec_descriptor = (mpf_codec_descriptor_t *) apr_palloc(pool,sizeof(mpf_codec_descriptor_t));
+//	mpf_codec_descriptor_init(codec_descriptor);
+//	codec_descriptor->channel_count = 1;
+//	codec_descriptor->payload_type = 96;
+//	apt_string_set(&codec_descriptor->name,"LPCM");
+//	codec_descriptor->sampling_rate = 16000;
 
 	synth_channel->flite_engine = (flite_synth_engine_t *) engine->obj;
 	synth_channel->speak_request = NULL; // no active speak request in progress
@@ -225,6 +220,13 @@ static mrcp_engine_channel_t* flite_synth_engine_channel_create(mrcp_resource_en
 	synth_channel->audio_buffer = NULL;
 	synth_channel->voice = NULL;
 	synth_channel->iId = 0;
+
+	if (apr_thread_mutex_create(&synth_channel->channel_guard,APR_THREAD_MUTEX_DEFAULT,pool) != APR_SUCCESS) 
+	{
+		apt_log(APT_LOG_MARK, APT_PRIO_ERROR, "Failed to create channel guard");
+		return NULL;
+	}
+
 
 	/* create engine channel base */
 	synth_channel->channel = mrcp_engine_source_channel_create(
@@ -253,22 +255,25 @@ static mrcp_engine_channel_t* flite_synth_engine_channel_create(mrcp_resource_en
 static apt_bool_t flite_synth_channel_destroy(mrcp_engine_channel_t *channel)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_destroy - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_destroy - channel %d", synth_channel->iId);
 	if(synth_channel->task)
 	{
 		apt_task_t *task = apt_consumer_task_base_get(synth_channel->task);
-		if (!apt_task_destroy(task))
+		if (!task || !apt_task_destroy(task))
 		{
 			apt_log(APT_LOG_MARK, APT_PRIO_WARNING, "Speak task destroy failed - channel %d", synth_channel->iId);
 		}
 		else
 		{
-			apt_log(APT_LOG_MARK, APT_PRIO_INFO, "Speak task destroyed - channel %d", synth_channel->iId);
+			apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "Speak task destroyed - channel %d", synth_channel->iId);
 		}
 	}
+	
 	synth_channel->task = NULL;
-
 	synth_channel->flite_engine->iChannels--;
+
+	apr_thread_mutex_destroy(synth_channel->channel_guard);
+
 	return TRUE;
 }
 
@@ -276,7 +281,7 @@ static apt_bool_t flite_synth_channel_destroy(mrcp_engine_channel_t *channel)
 static apt_bool_t flite_synth_channel_open(mrcp_engine_channel_t *channel)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_open - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_open - channel %d", synth_channel->iId);
 
 	synth_channel->voice = synth_channel->flite_engine->voices.awb;
 	return mrcp_engine_channel_open_respond(channel,TRUE);
@@ -286,7 +291,7 @@ static apt_bool_t flite_synth_channel_open(mrcp_engine_channel_t *channel)
 static apt_bool_t flite_synth_channel_close(mrcp_engine_channel_t *channel)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_close - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_close - channel %d", synth_channel->iId);
 	// create thread to wait for speak thread to terminate
 	flite_synth_channel_close_t(channel);
 	return TRUE; 
@@ -299,7 +304,7 @@ static apt_bool_t flite_synth_channel_request_process(mrcp_engine_channel_t *cha
 	mrcp_message_t *response = mrcp_response_create(request,request->pool);
 	apt_bool_t processed = FALSE;
 
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_request_process - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_request_process - channel %d", synth_channel->iId);
 
 	switch(request->start_line.method_id) {
 		case SYNTHESIZER_SET_PARAMS:
@@ -313,7 +318,7 @@ static apt_bool_t flite_synth_channel_request_process(mrcp_engine_channel_t *cha
 				//	} else if (!strcasecmp(voice_name, "slt")) {
 				//		synth_channel->voice = voices.slt;
 				//	} else {
-				//		apt_log(APT_LOG_MARK, APT_PRIO_INFO, "Valid voice names are awb, kal, rms or slt");
+				//		apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "Valid voice names are awb, kal, rms or slt");
 				//	}
 			break;
 		case SYNTHESIZER_GET_PARAMS:
@@ -351,7 +356,7 @@ static apt_bool_t flite_synth_channel_request_process(mrcp_engine_channel_t *cha
 static apt_bool_t flite_synth_channel_close_t(mrcp_engine_channel_t *channel)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_close_t - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_close_t - channel %d", synth_channel->iId);
 	
 	if (synth_channel->task) 
 	{
@@ -362,7 +367,7 @@ static apt_bool_t flite_synth_channel_close_t(mrcp_engine_channel_t *channel)
 		}
 		else
 		{
-			apt_log(APT_LOG_MARK, APT_PRIO_INFO, "Speak task terminated - channel %d", synth_channel->iId);
+			apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "Speak task terminated - channel %d", synth_channel->iId);
 			apt_task_destroy(task);
 			synth_channel->task = 0;
 		}
@@ -387,7 +392,7 @@ static apt_bool_t flite_synth_channel_speak(mrcp_engine_channel_t *channel, mrcp
 		flite_speak_msg_t *flite_msg = 0;
 
 		/* create task/thread to run flite so this function is not blocking */
-		apt_log(APT_LOG_MARK, APT_PRIO_NOTICE, "Create flite speak task - channel: %d", synth_channel->iId);
+		apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "Create flite speak task - channel: %d", synth_channel->iId);
 	    synth_channel->task = apt_consumer_task_create(synth_channel, msg_pool, synth_channel->pool);
 		if (!synth_channel->task) 
 		{
@@ -405,7 +410,7 @@ static apt_bool_t flite_synth_channel_speak(mrcp_engine_channel_t *channel, mrcp
 		task_vtable->process_msg = flite_speak;
 		synth_channel->speak_request = request;
 		
-		apt_log(APT_LOG_MARK,APT_PRIO_INFO, "Start task - channel %d", synth_channel->iId);
+		apt_log(APT_LOG_MARK,APT_PRIO_DEBUG, "Start task - channel %d", synth_channel->iId);
 		task = apt_consumer_task_base_get(synth_channel->task);
 		if (apt_task_start(task) == FALSE) 
 		{
@@ -419,7 +424,7 @@ static apt_bool_t flite_synth_channel_speak(mrcp_engine_channel_t *channel, mrcp
 		flite_msg = (flite_speak_msg_t*) msg->data;
 		flite_msg->channel = synth_channel;
 		flite_msg->request = request;
-		apt_log(APT_LOG_MARK,APT_PRIO_INFO, "Send signal to start speech synthesis - channel:%d", synth_channel->iId);
+		apt_log(APT_LOG_MARK,APT_PRIO_DEBUG, "Send signal to start speech synthesis - channel:%d", synth_channel->iId);
 		if (apt_task_msg_signal(task,msg))
 		{
 			response->start_line.request_state = MRCP_REQUEST_STATE_INPROGRESS;
@@ -427,7 +432,7 @@ static apt_bool_t flite_synth_channel_speak(mrcp_engine_channel_t *channel, mrcp
 		}
 		else
 		{
-			apt_log(APT_LOG_MARK,APT_PRIO_INFO, "Failed to send signal to start speech synthesis - channel:%d", synth_channel->iId);
+			apt_log(APT_LOG_MARK,APT_PRIO_WARNING, "Failed to send signal to start speech synthesis - channel:%d", synth_channel->iId);
 			apt_task_destroy(task);
 			return FALSE;
 		}
@@ -441,7 +446,13 @@ static apt_bool_t flite_speak(apt_task_t *task, apt_task_msg_t *msg)
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) flite_msg->channel;
 	apr_time_t start = 0;
 	apr_time_t elapsed = 0;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "< flite_speak_msg_process speak - channel %d", synth_channel->iId);
+	apr_time_t stamp = 0;
+
+	mrcp_message_t *stop_response = 0;
+	mpf_codec_t * codec = mrcp_engine_source_stream_codec_get(synth_channel->channel);
+	apr_uint16_t rate = codec->descriptor->sampling_rate;			
+
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "< flite_speak_msg_process speak - channel %d", synth_channel->iId);
 	
 	// just sequential stuff
 	start = apr_time_now();	// in microsec
@@ -450,28 +461,44 @@ static apt_bool_t flite_speak(apt_task_t *task, apt_task_msg_t *msg)
 		// TODO 
 		// create small units of text from synth_channel->speak_request->body.buf ( , . ? ! but ...
 		// synthesize small unit and store in audio_buffer
-		// check for pause/resume/stop 
+		// check for stop 
+		// pause resume state could improve performance 
+		// you can "pause" generating new speech from a unit of text 
+		// by checking the (decreasing) size of the audio_buffer
+		// no need to generate more speech samples than can be listened to...
 		cst_wave *wave = 0;
 		wave = flite_text_to_wave(synth_channel->speak_request->body.buf, synth_channel->voice);
 		if (wave && cst_wave_num_samples(wave)) 
 		{
+			int generated = (cst_wave_num_samples(wave)/cst_wave_sample_rate(wave)*1000);
+			stamp = apr_time_now();
+			elapsed = (stamp - start)/1000;
+			apt_log(APT_LOG_MARK, APT_PRIO_INFO, "TTS (chan %d) took %"APR_TIME_T_FMT" to generate %d of speech (in millisec)", synth_channel->iId, elapsed, generated);
+
+			if (rate != 16000)
+			{
+				cst_wave_resample(wave, rate);
+				elapsed = (apr_time_now() - stamp)/1000;
+				apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "TTS resampling to %d on (chan %d) took %"APR_TIME_T_FMT" millisec", rate, synth_channel->iId, elapsed);
+			}
 			mpf_buffer_audio_write(synth_channel->audio_buffer, cst_wave_samples(wave), cst_wave_num_samples(wave) * 2);
 			delete_wave(wave);
 		}
 	}
 
-	elapsed = (apr_time_now() - start)/1000;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "TTS (chan %d) took %"APR_TIME_T_FMT" millisec", synth_channel->iId, elapsed);
+	apr_thread_mutex_lock(synth_channel->channel_guard);
+	stop_response = synth_channel->stop_response; 
+	apr_thread_mutex_unlock(synth_channel->channel_guard);
 
-	if (!synth_channel->stop_response)
+	if (!stop_response)
 	{
 		// this will notify the callback that feeds the client that synthesis is complete
 		mpf_buffer_event_write(synth_channel->audio_buffer, MEDIA_FRAME_TYPE_EVENT);
-		apt_log(APT_LOG_MARK, APT_PRIO_INFO, "> flite_speak_msg_process speak - end of TTS - %d", synth_channel->iId);
+		apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "> flite_speak_msg_process speak - end of TTS - %d", synth_channel->iId);
 	}
 	else
 	{
-		apt_log(APT_LOG_MARK, APT_PRIO_INFO, "> flite_speak_msg_process speak - channel %d", synth_channel->iId);
+		apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "> flite_speak_msg_process speak - channel %d", synth_channel->iId);
 	}
 	return TRUE;
 }
@@ -480,11 +507,13 @@ static apt_bool_t flite_speak(apt_task_t *task, apt_task_msg_t *msg)
 static apt_bool_t flite_synth_channel_stop(mrcp_engine_channel_t *channel, mrcp_message_t *request, mrcp_message_t *response)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_stop - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_stop - channel %d", synth_channel->iId);
 	/* store the request, make sure there is no more activity and only then send the response */
 
-	/* TODO this should probably be mutexed */
+	apr_thread_mutex_lock(synth_channel->channel_guard);
 	synth_channel->stop_response = response;
+	apr_thread_mutex_unlock(synth_channel->channel_guard);
+
 	return TRUE;
 }
 
@@ -492,7 +521,7 @@ static apt_bool_t flite_synth_channel_stop(mrcp_engine_channel_t *channel, mrcp_
 static apt_bool_t flite_synth_channel_pause(mrcp_engine_channel_t *channel, mrcp_message_t *request, mrcp_message_t *response)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_pause - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_pause - channel %d", synth_channel->iId);
 	
 	synth_channel->paused = TRUE;
 	/* send asynchronous response */
@@ -504,7 +533,7 @@ static apt_bool_t flite_synth_channel_pause(mrcp_engine_channel_t *channel, mrcp
 static apt_bool_t flite_synth_channel_resume(mrcp_engine_channel_t *channel, mrcp_message_t *request, mrcp_message_t *response)
 {
 	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) channel->method_obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_channel_resume - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_channel_resume - channel %d", synth_channel->iId);
 
 	synth_channel->paused = FALSE;
 	/* send asynchronous response */
@@ -522,16 +551,16 @@ static apt_bool_t flite_synth_stream_destroy(mpf_audio_stream_t *stream)
 /** Callback is called from MPF engine context to perform any action before open */
 static apt_bool_t flite_synth_stream_open(mpf_audio_stream_t *stream)
 {
-	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) stream->obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_stream_open - channel %d", synth_channel->iId);
+//	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) stream->obj;
+//	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_stream_open - channel %d", synth_channel->iId);
 	return TRUE;
 }
 
 /** Callback is called from MPF engine context to perform any action after close */
 static apt_bool_t flite_synth_stream_close(mpf_audio_stream_t *stream)
 {
-	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) stream->obj;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_stream_close - channel %d", synth_channel->iId);
+//	flite_synth_channel_t *synth_channel = (flite_synth_channel_t *) stream->obj;
+//	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_stream_close - channel %d", synth_channel->iId);
 	return TRUE;
 }
 
@@ -540,7 +569,7 @@ static apt_bool_t flite_synth_speak_complete_raise(flite_synth_channel_t *synth_
 {
 	mrcp_message_t *message = 0;
 	mrcp_synth_header_t * synth_header = 0;
-	apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_speak_complete_raise - channel %d", synth_channel->iId);
+	apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_speak_complete_raise - channel %d", synth_channel->iId);
 
 	if (!synth_channel->speak_request) {
 		return FALSE;
@@ -587,7 +616,7 @@ static apt_bool_t flite_synth_stream_read(mpf_audio_stream_t *stream, mpf_frame_
 	{
 		/* normal processing */
 		mpf_buffer_frame_read(synth_channel->audio_buffer,frame);
-//		apt_log(APT_LOG_MARK, APT_PRIO_INFO, "flite_synth_stream_read - channel %d - size %d", synth_channel->iId, mpf_buffer_get_size(synth_channel->audio_buffer));
+//		apt_log(APT_LOG_MARK, APT_PRIO_DEBUG, "flite_synth_stream_read - channel %d - size %d", synth_channel->iId, mpf_buffer_get_size(synth_channel->audio_buffer));
 
 		if((frame->type & MEDIA_FRAME_TYPE_EVENT) == MEDIA_FRAME_TYPE_EVENT) 
 		{
