3a560779943652db8a01d867356ce040eba627a0
Arsen Chaloyan <achaloyan@gmail.com>
2009-08-10 05:40:10 +0000

    Initialized codec manager with 4 instead of 3 codecs to avoid later reallocations, as we actually have 4 codecs now

--- libs/mpf/src/mpf_engine.c
+++ libs/mpf/src/mpf_engine.c
@@ -286,7 +286,7 @@ static void mpf_engine_main(mpf_timer_t *timer, void *data)
 
 MPF_DECLARE(mpf_codec_manager_t*) mpf_engine_codec_manager_create(apr_pool_t *pool)
 {
-	mpf_codec_manager_t *codec_manager = mpf_codec_manager_create(3,pool);
+	mpf_codec_manager_t *codec_manager = mpf_codec_manager_create(4,pool);
 	if(codec_manager) {
 		mpf_codec_t *codec;
 		codec = mpf_codec_lpcm_create(pool);
