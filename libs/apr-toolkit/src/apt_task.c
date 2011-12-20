8f77c61c0e2c2c7047e949a8be55feab5869f937
Arsen Chaloyan <achaloyan@gmail.com>
2011-12-20 03:45:12 +0000

    Enhanced debug output by adding apt_task_msg_t identifier (pointer) to "Signal Message" and "Process Message" log statements to be able to match signaled and processed messages.

--- libs/apr-toolkit/src/apt_task.c
+++ libs/apr-toolkit/src/apt_task.c
@@ -226,8 +226,8 @@ APT_DECLARE(apt_task_msg_t*) apt_task_msg_get(apt_task_t *task)
 
 APT_DECLARE(apt_bool_t) apt_task_msg_signal(apt_task_t *task, apt_task_msg_t *msg)
 {
-	apt_log(APT_LOG_MARK,APT_PRIO_DEBUG,"Signal Message to [%s] [%d;%d]",
-		task->name, msg->type, msg->sub_type);
+	apt_log(APT_LOG_MARK,APT_PRIO_DEBUG,"Signal Message to [%s] [0x%x;%d;%d]",
+		task->name, msg, msg->type, msg->sub_type);
 	if(task->vtable.signal_msg) {
 		return task->vtable.signal_msg(task,msg);
 	}
@@ -238,6 +238,8 @@ APT_DECLARE(apt_bool_t) apt_task_msg_parent_signal(apt_task_t *task, apt_task_ms
 {
 	apt_task_t *parent_task = task->parent_task;
 	if(parent_task) {
+		apt_log(APT_LOG_MARK,APT_PRIO_DEBUG,"Signal Message to [%s] [0x%x;%d;%d]",
+			parent_task->name, msg, msg->type, msg->sub_type);
 		if(parent_task->vtable.signal_msg) {
 			return parent_task->vtable.signal_msg(parent_task,msg);
 		}
@@ -275,8 +277,8 @@ static apt_bool_t apt_core_task_msg_process(apt_task_t *task, apt_task_msg_t *ms
 APT_DECLARE(apt_bool_t) apt_task_msg_process(apt_task_t *task, apt_task_msg_t *msg)
 {
 	apt_bool_t status = FALSE;
-	apt_log(APT_LOG_MARK,APT_PRIO_DEBUG,"Process Message [%s] [%d;%d]",
-		task->name, msg->type, msg->sub_type);
+	apt_log(APT_LOG_MARK,APT_PRIO_DEBUG,"Process Message [%s] [0x%x;%d;%d]",
+		task->name, msg, msg->type, msg->sub_type);
 	if(msg->type == TASK_MSG_CORE) {
 		status = apt_core_task_msg_process(task,msg);
 	}
